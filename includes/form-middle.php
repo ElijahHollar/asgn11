b0a18224c8c2c26a87233bf412e535b9e482add2
Elijah Hollar <elijahshollar@students.abtech.edu>
2020-04-24 17:39:16 -0400

    Removed unnecessary include files, minor code cleanup

--- includes/form-middle.php
+++
@@ -1,8 +0,0 @@
-
-</select>
-        </div>
-        
-        <div class="entry">
-          <label>To:</label>&nbsp;
-          <input type="text" name="toValue" value="<?php echo $toValue; ?>">&nbsp;
-          <select name="toUnit">
